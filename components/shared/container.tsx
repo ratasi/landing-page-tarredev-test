0a620c44041ce457819d75a4d6a154d07fd7cb76
Rafa Tárrega <rafatarre@gmail.com>
2024-02-09 19:53:12 +0100

    feat: container paddings

--- components/shared/container.tsx
+++ components/shared/container.tsx
@@ -6,7 +6,7 @@ const Container = (props: ContainerProps) => {
     const { children } = props
 
     return (
-        <div className="px-12 py-20 md:px-40 md:py-36 max-w-3xl mx-auto">
+        <div className="px-12 py-20 md:px-40 md:py-32 max-w-3xl mx-auto">
             {children}
         </div>
     );
